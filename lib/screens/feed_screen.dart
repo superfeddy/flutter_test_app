0afff4dd9f938af562ddaedb4da50a30b4dc1a58
superfeddy <fedf0415@gmail.com>
2023-02-03 18:52:41 +0000

    Added feed screen and fetched user data for profile screen

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -1,9 +1,28 @@
 import 'package:flutter/material.dart';
 
+// constants
+const baseImageUrl = 'https://picsum.photos/';
+const imageWidth = 250;
+const imageHeight = 200;
+const imageCount = 10;
+
 class FeedScreen extends StatelessWidget {
   const FeedScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Feed Screen'));
+    return ListView.builder(
+        padding: const EdgeInsets.all(8),
+        itemCount: imageCount,
+        itemBuilder: (BuildContext context, int index) {
+          return Padding(
+            padding: const EdgeInsets.all(15),
+            child: Column(
+              children: <Widget>[
+                Image.network(
+                    '$baseImageUrl$imageWidth/$imageHeight?random=$index'),
+              ],
+            ),
+          );
+        });
   }
 }
--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,7 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-class ProfileScreen extends StatelessWidget {
+import 'dart:async';
+import 'dart:convert';
+import 'dart:math';
+
+const apiUrl = 'https://reqres.in/api/users/';
+const userCount = 12;
+
+class User {
+  final int id;
+  final String email;
+  final String firstName;
+  final String lastName;
+  final String avatar;
+
+  const User({
+    required this.id,
+    required this.email,
+    required this.firstName,
+    required this.lastName,
+    required this.avatar,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['data']['id'],
+      email: json['data']['email'],
+      firstName: json['data']['first_name'],
+      lastName: json['data']['last_name'],
+      avatar: json['data']['avatar'],
+    );
+  }
+}
+
+Future<User> fetchUser() async {
+  final response = await http
+      .get(Uri.parse('$apiUrl${Random().nextInt(userCount - 1) + 1}'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return User.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load user');
+  }
+}
+
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  late Future<User> futureUser;
+
+  @override
+  void initState() {
+    super.initState();
+    futureUser = fetchUser();
+    futureUser.then((value) => print(value.email));
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Center(child: Text('Profile Screen'));
